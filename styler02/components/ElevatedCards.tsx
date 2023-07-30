2db2b0d9182bb30619ed1ab936c3ac712a4b7db8
thenishantgiri <nishantkrgiri@gmail.com>
2023-07-31 04:35:19 +0530

    Project02: Horizontal Elevated Cards Added

---
+++ styler02/components/ElevatedCards.tsx
@@ -0,0 +1,64 @@
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function ElevetedCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Elevated Cards</Text>
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={styles.container}>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>Tap</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>me</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>to</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>scroll</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>more...</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>☺️</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardElevated: {
+    backgroundColor: '#CAD5E2',
+    elevation: 4,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: 'red',
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+  },
+});
