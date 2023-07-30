528ab606f05704007dd6ad4ddd0a30476858c796
thenishantgiri <nishantkrgiri@gmail.com>
2023-07-31 03:08:46 +0530

    Project01: completed

---
+++ xBetaProgram/AppPro.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Text, View, StyleSheet, useColorScheme} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        Hello, from AppPro!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: 'white',
+  },
+  darkText: {
+    color: 'black',
+  },
+});
+
+export default AppPro;
